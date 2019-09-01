9878e5a7b0986ac530de344667a6705a44065746
lalit duggal <Lalit.duggal@gmail.com>
2019-09-01 23:28:23 +1000

    Update lambdademofunction.py

--- lambdademofunction.py
+++ lambdademofunction.py
@@ -4,5 +4,5 @@ def lambda_handler(event, context):
     # TODO implement
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello Lalit Duggal')
+        'body': json.dumps('Hello Lalit Duggal for MYOB Demo')
     }
